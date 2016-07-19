c988659148206636ea3a76d4b55d54241235e162
lyleKP <v2top1@163.com>
2016-07-19 14:51:07 +0800

    Initial Commit

---
+++ C_Study/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  C_Study
+//
+//  Created by lyleKP on 16/7/19.
+//  Copyright © 2016年 lyleKP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
